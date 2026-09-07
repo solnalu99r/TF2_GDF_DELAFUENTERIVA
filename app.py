e9794506145eec1079de6007c1d9eecada74b933
solnalu99r <solnalu.dlf.riva@gmail.com>
2026-09-07 05:52:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ st.markdown(
         display: none !important;
     }
     .st-key-logo_chico_top {
-        margin-top: 2.8rem !important;
+        margin-top: 3 rem !important;
     }
     </style>
     """,
